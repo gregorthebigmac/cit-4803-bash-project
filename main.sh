0782d2a4b0661f58d44e7dcb465ec772cca28fb6
gregorthebigmac@gmail.com <gregorthebigmac@gmail.com>
2019-03-20 12:25:42 -0500

    added chgpass.sh file as well as more high-level stuff in main.sh

--- main.sh
+++ main.sh
@@ -5,6 +5,11 @@
 # High level description:
 # 1. Print welcome splash, a few helpful commands and how to access "help"
 # 2. main_loop()
-#	[todo] create list of commands
-#	a. Get user command
-#	
+#	a. Get user command and args. Is command valid?
+#		1. YES	-> Check args. Are they valid?
+#			a. YES	-> Check permissions. Does the user have permissions to perform the action?
+#				1. YES	-> Run command and args (see commands.txt for descriptions)
+#				2. NO	-> Fail with "access denied" message.
+#			b. NO	-> Fail with [command] usage page (see commands.txt)
+#		2. NO	-> Fail with message "[command] not found."
+#	b. continue main_loop()
