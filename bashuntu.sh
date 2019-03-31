450ed259a4294cd1a1a5edc07a1e0cf7bdce168a
gregorthebigmac <gregorthebigmac@gmail.com>
2019-03-31 16:01:54 -0500

    Got some basics done. Still doesn't set the username in the text file, but it checks for validity of the username.

--- bashuntu.sh
+++ bashuntu.sh
@@ -1,9 +1,23 @@
 #!/bin/bash
 
-# This serves as the main entry point to the script.
+# This serves as the main entry point to the script. For the time being (i.e. for the group's benefit)
+# I'm going to leave the high-level "structure and flow" comments in place, with the code that does that
+# comment's task immediately after. For instance, if a comment read "echo result back to user" then the code
+# would look like:
+
+# Echo result back to user
+#echo "$result"
+
+# Okay, onto the script itself.
 
 # High level description:
-# 1. Check file for admin account
-#	a. Does account "admin" exist?
-#		1. YES	-> admin account needs to be created. Run setup_admin.sh
-#		2. NO 	-> admin account has been created. Run login.sh
+does_admin_account_exist=$(cat users.txt | grep "admin")	# 1. Check file for admin account
+
+if [[ $does_admin_account_exist == *"admin"* ]]; then	# 2. Does account "admin" exist?
+  echo "WARNING! Admin account has not been setup yet! Creating Admin account now..."
+  ./setup_admin.sh	# 2a. YES	-> admin account needs to be created. Run setup_admin.sh
+else
+  echo "Initializing shell...[ok]"
+  echo "Proceeding to login screen..."
+  ./login.sh	# 2b. NO 	-> admin account has been created. Run login.sh
+fi
--- setup_admin.sh
+++ setup_admin.sh
@@ -4,7 +4,38 @@
 
 # High Level Description:
 # 1. Prompt the admin to create a username
-#	a. It CANNOT be "admin" or "root"
+user_is_valid=false
+while [ $user_is_valid == false ]; do
+  echo "Usernames MUST be all lowercase"
+  echo "Usernames may NOT contain spaces or special characters."
+  echo "Please enter your new username:"
+  read username
+  echo "You put $username. Is this correct? y/n"
+  read confirm
+  if [ $confirm == "y" ] || [ $confirm == "Y" ]; then
+	# 1a. It CANNOT be "admin" or "root" or "user"
+	# 1b. It CANNOT contain spaces or special characters
+    pattern=" |'"
+    if [[ $username =~ [A-Z] ]]; then
+	  echo "You entered $username. Usernames CANNOT contain uppercase letters. Try again."
+	elif [[ $username =~ $pattern ]]; then
+	    echo "Usernames CANNOT contain spaces! And don't think you can trick me by putting it in quotes! Try again."
+	elif [[ $username == *"admin"* ]]; then
+	  echo "Your username cannot be \"admin\", nor can it *contain* the word \"admin\". Try again."
+	elif [[ $username == *"root"* ]]; then
+	  echo "Don't make your username \"root\" or contain root in the name in any way. That's too easy to guess. Try again."
+	elif [[ $username == *"user"* ]]; then
+	  echo "I thought you were trying to keep people from guessing your username? Don't use \"user\" as a username. Try again."
+	elif [[ $username == *['!'@\#\$%^\&*()_+]* ]]; then
+	  echo "What part of NO SPECIAL CHARACTERS did you not understand? Try again."
+	else
+	  user_is_valid=true
+	  echo "Great! Your username is now $username. Now let's create a password..."
+	fi
+  else echo "Okay, try again."
+  fi
+done
+
 # 2. Prompt the admin to create a password
 #	b. [todo] should we enforce tough password?
 #		1. Kyle: I'm leaning towards YES
