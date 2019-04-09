b135e3aa4feeb393b046ef0e6e548e483cc1b33f
Gregor <gregorthebigmac@gmail.com>
2019-04-08 23:27:17 -0500

    overhauled the file system layout for better organization and clearer marking of what goes where.

--- login_screen.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# This file will contain the login screen, prompting the user to enter their
-#	username and password, and will check it against the stored credentials
-#	and either grant access to the user or deny them if the credentials are
-#	incorrect.
-
-echo "test"
