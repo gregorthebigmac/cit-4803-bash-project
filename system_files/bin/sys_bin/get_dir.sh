acfdc5cba0bfdb3cf3a945a5e8119ba855071aeb
gregorthebigmac <gregorthebigmac@gmail.com>
2019-04-22 08:23:20 -0500

    committing what I have now before I start a new branch.

---
+++ system_files/bin/sys_bin/get_dir.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+get_shell_dir() {
+	working_dir="$(cat "$root_dir/system_files/bin/sys_bin/.current_dir")"
+	IFS='/'
+	read -ra ADDR <<< "$working_dir"
+	root_dir_ctr=0
+	this_dir=""
+	for i in "${ADDR[@]}"; do
+		if ((root_dir_ctr == 0)); then
+			if [[ "$i" == "file_system" ]]; then
+				root_dir_ctr=$((root_dir_ctr + 1))
+				this_dir="/"
+			fi
+		else this_dir="$this_dir/$i"
+		fi
+	done
+	echo -n "$this_dir"
+}
