12fc4ff68525549cdf89a19ecf3e10538d2e9932
gregorthebigmac <gregorthebigmac@gmail.com>
2019-04-23 00:39:50 -0500

    spotted and fixed a nasty bug in cd!

--- system_files/bin/sys_bin/get_dir.sh
+++ system_files/bin/sys_bin/get_dir.sh
@@ -7,12 +7,15 @@ get_shell_dir() {
 	root_dir_ctr=0
 	this_dir=""
 	for i in "${ADDR[@]}"; do
-		if ((root_dir_ctr == 0)); then
-			if [[ "$i" == "file_system" ]]; then
-				root_dir_ctr=$((root_dir_ctr + 1))
-				this_dir="/"
-			fi
-		else this_dir="$this_dir$i"
+		if ((root_dir_ctr == 1)); then
+			this_dir="$this_dir$i"
+			root_dir_ctr=$((root_dir_ctr + 1))
+		elif ((root_dir_ctr > 1)); then
+			this_dir="$this_dir/$i"
+		fi
+		if [[ "$i" == "file_system" ]]; then
+			root_dir_ctr=$((root_dir_ctr + 1))
+			this_dir="/"
 		fi
 	done
 	echo -n "$this_dir"
